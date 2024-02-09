ec4c806109c36228415d2e59f6877b969020d73f
Dpbm <dpbm136@gmail.com>
2024-02-09 10:12:36 -0300

    added hello 2

---
+++ terceiro-ano/dotnet/hello-2/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace hello_2;
+
+public class Fibo{
+	public int f(int n){
+		if(n<=1)
+			return 1;
+		return f(n-1)+f(n-2);
+	}
+}
+
+public class Program
+{
+
+    static void Main(string[] args)
+    {
+	Fibo fib = new Fibo();	
+        Console.WriteLine(fib.f(10));
+    }	
+}
+
